00936d8a4c534eea1fb4f24cd606e0b13feb0e82
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-23 11:47:38 -0700

    added prompt

---
+++ script.js
@@ -0,0 +1,15 @@
+var loss = 0;
+var wins = 0;
+var ties = 0;
+
+
+
+var gamechoices = ["R", "P", "S"];
+
+var gameOn = function() {
+    prompt("Select R, P, S");
+
+  if(selection) {
+      return;
+  }
+}
